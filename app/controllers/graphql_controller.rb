4f854ed1c7460f26551bc7501e54812b598b51ac
Sergey Antonov <sergey.antonov@flatstack.com>
2020-04-06 13:26:19 +0300

    Quality fix

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -37,28 +37,23 @@ class GraphqlController < ApplicationController
     params[:operationName]
   end
 
-  # Handle form data, JSON body, or a blank value
   def ensure_hash(ambiguous_param)
     case ambiguous_param
     when String
-      if ambiguous_param.present?
-        ensure_hash(JSON.parse(ambiguous_param))
-      else
-        {}
-      end
-    when Hash, ActionController::Parameters
-      ambiguous_param
-    when nil
-      {}
+      ambiguous_param.present? ? ensure_hash(JSON.parse(ambiguous_param)) : {}
+    when Hash, ActionController::Parameters then ambiguous_param
+    when nil then {}
     else
       raise ArgumentError, "Unexpected parameter: #{ambiguous_param}"
     end
   end
 
-  def handle_error_in_development(e)
-    logger.error e.message
-    logger.error e.backtrace.join("\n")
+  def handle_error_in_development(error)
+    logger.error error.message
+    logger.error error.backtrace.join("\n")
 
-    render json: { error: { message: e.message, backtrace: e.backtrace }, data: {} }, status: 500
+    response_body = { error: { message: error.message, backtrace: error.backtrace }, data: {} }
+
+    render json: response_body, status: 500
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::Whitelist
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-         :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+    :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
   validates :password, confirmation: true
   validates :password_confirmation, presence: true, on: :create
