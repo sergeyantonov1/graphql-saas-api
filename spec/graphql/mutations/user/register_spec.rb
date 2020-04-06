837226c7ef4936353aab15a5acb5b92aa8926645
Sergey Antonov <sergey.antonov@flatstack.com>
2020-04-06 13:54:55 +0300

    WIP

---
+++ spec/graphql/mutations/user/register_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe Mutations::User::Register do
+  let(:query) do
+    <<-GRAPHQL
+      mutation {
+        register(
+          input: {
+            email: "user@example.com",
+            password: "password",
+            passwordConfirmation: "password"
+          }
+        ) {
+          token
+        }
+      }
+    GRAPHQL
+  end
+
+  it_behaves_like :graphql_request, "register user" do
+    let(:fixture_path) { "json/graphql/mutations/user/register.json" }
+  end
+end
