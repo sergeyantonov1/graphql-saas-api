60cdbabbd9efb6d805bedb24a76768a423e2ec9f
Sergey Antonov <sergey.antonov@flatstack.com>
2020-05-17 15:35:14 +0300

    fix

--- app/policies/invitation_policy.rb
+++ app/policies/invitation_policy.rb
@@ -3,7 +3,9 @@
 class InvitationPolicy < ApplicationPolicy
   authorize :company, allow_nil: true
 
+  delegate :id, to: :company, prefix: true, allow_nil: true
+
   def create?
-    user.own_companies.exists?(company.id)
+    user.own_companies.exists?(company_id)
   end
 end
